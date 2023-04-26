d4fed1a32a086dd622ebb9ee85e1ad7b70750f87
Fisayo Oladejo <108827240+FisayoOladejo@users.noreply.github.com>
2023-04-26 21:14:08 +0100

    A script that computes the number of tasks completed by user id.

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request(process.argv[2], function (err, _res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const completedTasksByUsers = {};
+    body = JSON.parse(body);
+
+    for (let i = 0; i < body.length; ++i) {
+      const userId = body[i].userId;
+      const completed = body[i].completed;
+
+      if (completed && !completedTasksByUsers[userId]) {
+        completedTasksByUsers[userId] = 0;
+      }
+
+      if (completed) ++completedTasksByUsers[userId];
+    }
+
+    console.log(completedTasksByUsers);
+  }
+});
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Pri6-completed_tasks.js
